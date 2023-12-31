9fb83551f94af0dd76b32853af9f228efefd8707
RajuNiranjan <rajuniranjan1910@gmail.com>
2023-12-31 09:42:22 +0530

    created headder and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
+import Headder from "./components/Headder";
 
 const App = () => {
   return (
     <Router>
+      <Headder />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
